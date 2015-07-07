9f6d1c494e77d6f3ba5879bd11495d35f59f6224
JR Powers-Luhn <floobyt@gmail.com>
2015-07-07 13:01:16 -0500

    First unit test to verify URL mapping

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,8 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # Commented out admin site
+    # url(r'^admin/', include(admin.site.urls)),
 )
