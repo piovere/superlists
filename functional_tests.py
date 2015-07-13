32777d2d5828b8af72ca1782f97e9ccd2391fb0b
JR Powers-Luhn <floobyt@gmail.com>
2015-07-13 16:13:23 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -17,16 +17,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		# self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# The app says to enter an item!
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		# I type in "Water the garden"
+		inputbox.send_keys('Water the garden')
 
 		# When I hit enter, the page updates and the page lists "1: Water the garden" as an item on the to do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Water the garden' for row in rows)
+		)
 
 		# There's still a text box
 		# I enter "Fly spray Jesse"
+		self.fail('Finish the test!')
 
 		# The page updates again and then both items show up
 
