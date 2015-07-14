a5b6c30a328a2fb26855ff6b46cf30d952762970
JR Powers-Luhn <floobyt@gmail.com>
2015-07-13 22:10:08 -0500

    refactored test to use common in_table_check method

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_new_item')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
@@ -34,9 +39,7 @@ class NewVisitorTest(unittest.TestCase):
 		# When I hit enter, the page updates and the page lists "1: Water the garden" as an item on the to do list
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Water the garden', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Water the garden')
 
 		# There's still a text box
 		# I enter "Fly spray Jesse"
@@ -45,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again and then both items show up
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Water the garden', [row.text for row in rows])
-		self.assertIn('2: Fly spray Jesse', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Water the garden')
+		self.check_for_row_in_list_table('2: Fly spray Jesse')
 
 
 		# There is some text telling me that I have my own special URL to visit
